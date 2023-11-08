6499a17cd71be20fff355a6640a997048cb18276
root <molinasosacarolina17@gmail.com>
2023-11-07 20:02:37 -0800

    Add UserImage and change some functionalities and style

--- campaigns.js
+++ campaigns.js
@@ -1,90 +1,109 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('addCampaignBtn').addEventListener('click', function() {
-        document.getElementById('campaignForm').classList.remove('hidden');
+        toggleFormVisibility();
     });
 
-    document.getElementById('campaignFormElement').addEventListener('submit', submitCampaignForm);
+    document.getElementById('campaignFormElement').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const isEditing = this.dataset.isEditing === 'true';
+        const campaignData = {
+            title: document.getElementById('title').value,
+            description: document.getElementById('description').value,
+            videoUrl: document.getElementById('videoUrl').value
+        };
+        if (isEditing) {
+            updateCampaign(campaignData);
+        } else {
+            addCampaignToDOM(campaignData);
+        }
+        hideForm();
+    });
+
+    document.getElementById('dashboard').addEventListener('click', function() {
+        window.location.href = './index.html';
+    });
+
+    document.getElementById('statistics').addEventListener('click', function() {
+        window.location.href = './StatisticsPage.html';
+    });
+
+    document.getElementById('campaignsBtn').classList.add('active');
+    hideForm();
 });
 
-function submitCampaignForm(event) {
-    event.preventDefault();
-    // Logic to handle the form submission
-    // Add the new campaign to the campaignList
-    // Hide the form again
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-    const videoUrl = document.getElementById('videoUrl').value;
+function toggleFormVisibility() {
+    const form = document.getElementById('campaignForm');
+    form.classList.toggle('hidden');
+    form.dataset.isEditing = 'false';
+    resetForm();
+}
 
-    addCampaignToDOM(title, description, videoUrl);
+function hideForm() {
     document.getElementById('campaignForm').classList.add('hidden');
 }
 
-function addCampaignToDOM(title, description, videoUrl) {
+function resetForm() {
+    const form = document.getElementById('campaignFormElement');
+    form.reset();
+    form.dataset.isEditing = 'false';
+    form.dataset.editingTitle = '';
+}
+
+function addCampaignToDOM(campaign) {
     const campaignList = document.getElementById('campaignList');
     const li = document.createElement('li');
-    
+    li.className = 'box';
     li.innerHTML = `
-        <h3>${title}</h3>
-        <p>${description}</p>
-        <a href="${videoUrl}" target="_blank">Watch Video</a>
-        <button onclick="editCampaign('${title}');">Edit</button>
-        <button onclick="deleteCampaign('${title}');">Delete</button>
+        <h3>${campaign.title}</h3>
+        <p>${campaign.description}</p>
+        <a href="${campaign.videoUrl}" target="_blank">Watch Video</a>
+        <button onclick="editCampaign('${campaign.title.replace(/'/g, "\\'")}');">Edit</button>
+        <button class="delete-button">Delete</button>
     `;
-    
     campaignList.appendChild(li);
+
+    li.querySelector('.delete-button').addEventListener('click', () => {
+        deleteCampaign(li);
+    });
 }
 
 function editCampaign(title) {
     const campaigns = document.querySelectorAll('#campaignList li');
     campaigns.forEach(campaign => {
         if (campaign.querySelector('h3').textContent === title) {
-            // Populate form with existing values
             document.getElementById('title').value = title;
             document.getElementById('description').value = campaign.querySelector('p').textContent;
             document.getElementById('videoUrl').value = campaign.querySelector('a').href;
             
-            // Display the form
+            const campaignForm = document.getElementById('campaignFormElement');
+            campaignForm.dataset.isEditing = 'true';
+            campaignForm.dataset.editingTitle = title;
+
             document.getElementById('campaignForm').classList.remove('hidden');
         }
     });
 }
 
-
-function deleteCampaign(title) {
+function updateCampaign(newData) {
     const campaigns = document.querySelectorAll('#campaignList li');
+    const campaignForm = document.getElementById('campaignFormElement');
+    const editingTitle = campaignForm.dataset.editingTitle;
+
     campaigns.forEach(campaign => {
-        if (campaign.querySelector('h3').textContent === title) {
-            campaign.remove();
+        if (campaign.querySelector('h3').textContent === editingTitle) {
+            campaign.querySelector('h3').textContent = newData.title;
+            campaign.querySelector('p').textContent = newData.description;
+            campaign.querySelector('a').href = newData.videoUrl;
+
+            // Update the edit button to pass the new title
+            campaign.querySelector('button').setAttribute('onclick', `editCampaign('${newData.title.replace(/'/g, "\\'")}');`);
         }
     });
+
+    campaignForm.dataset.isEditing = 'false';
+    campaignForm.dataset.editingTitle = '';
 }
 
-document.querySelectorAll('header nav button').forEach(btn => {
-    btn.addEventListener('click', function() {
-        // Remove active class from all buttons
-        document.querySelectorAll('header nav button').forEach(button => {
-            button.classList.remove('active');
-        });
-        // Add active class to clicked button
-        this.classList.add('active');
-
-        // Optional: Show the related section only
-        showSection(this.id);
-    });
-});
-
-// Function to handle showing the correct section
-function showSection(sectionId) {
-    // Assuming you have sections with corresponding IDs
-    // Hide all sections
-    document.querySelectorAll('main > div').forEach(section => {
-        section.classList.add('hidden');
-    });
-    // Show the clicked section
-    document.querySelector(`#${sectionId}`).classList.remove('hidden');
-}
-
-// Set the campaigns button as active on page load
-window.onload = () => {
-    document.getElementById('campaignsBtn').classList.add('active');
+function deleteCampaign(li) {
+    li.remove();
 }
