69f41b77e3f319fa833038be397368a47fdce1aa
4mmZ <andresmaxi111014@gmail.com>
2023-11-13 12:08:49 -0300

    cambios que hicimos para q funcione

--- backend/router/process_engine.py
+++ backend/router/process_engine.py
@@ -27,7 +27,7 @@ def load_img(user_id):
         db = get_connection()
         cur = db.cursor()
 
-        cur.execute("SELECT frames_data FROM to_process_img WHERE user_id = %s", (user_id,))
+        cur.execute("SELECT frames_data FROM to_process_img WHERE video_id = %s", (user_id,))
         row = cur.fetchone()
 
         if row is None:
@@ -58,114 +58,130 @@ def load_img(user_id):
         cur.close()
         db.close()
 
-# @router.route("/load_model/<int:user_id>")
-# def get_model(user_id):
-#     def folder_load(folder_path, height=48, width=48):
-#         imagenes = []
-#         new_folder_path = 'images/to_process_images'
-#         if not os.path.exists(new_folder_path):
-#             os.makedirs(new_folder_path)
+@router.route("/load_model/<int:user_id>")
+def get_model(user_id):
+    from ultralytics import YOLO
 
-#         for filename in os.listdir(folder_path):
-#             if filename.endswith(".jpg") or filename.endswith(".png"):
-#                 img_path = os.path.join(folder_path, filename)
-#                 imagenes.append(img_path)
+    def folder_load(folder_path, height=48, width=48):
+        imagenes = []
+        new_folder_path = 'images/to_process_images'
+        if not os.path.exists(new_folder_path):
+            os.makedirs(new_folder_path)
+            
+            for filename in os.listdir(folder_path):
+                if filename.endswith(".jpg") or filename.endswith(".png"):
+                    img_path = os.path.join(folder_path, filename)
+                    imagenes.append(img_path)
 
-#         for counter, img_path2 in enumerate(imagenes):  # Utilizamos enumerate para obtener un contador único
-#             with Image.open(img_path2) as img:
-#                 img_resized = img.resize((height, width))
+            for counter, img_path2 in enumerate(imagenes):  # Utilizamos enumerate para obtener un contador único
+                with Image.open(img_path2) as img:
+                    img_resized = img.resize((height, width))
 
-#                 file_name, extension = os.path.splitext(os.path.basename(img_path2))
-#                 # Reemplaza espacios con guiones bajos y elimina caracteres no deseados
-#                 file_name = file_name.replace(" ", "_").replace("-", "_")
-#                 new_path = os.path.join(new_folder_path, f'{file_name}_{counter}_{height}x{width}{extension}')
-#                 img_resized.save(new_path)
+                file_name, extension = os.path.splitext(os.path.basename(img_path2))
+                 # Reemplaza espacios con guiones bajos y elimina caracteres no deseados
+                file_name = file_name.replace(" ", "_").replace("-", "_")
+                new_path = os.path.join(new_folder_path, f'{file_name}_{counter}_{height}x{width}{extension}')
+                img_resized.save(new_path)
 
-#         return new_folder_path
+        return new_folder_path
     
-#     folder_load("images_/captured_frames/{}".format(user_id))
+    folder_load("images_/captured_frames/{}".format(user_id))
     
-    
-#     try:
-#         model = YOLO('./models/emilio_v1.pt')
-#         class_ids = []
-#         image_folder = "images_/captured_frames/{}".format(user_id)
-
-#         for filename in os.listdir(image_folder):
-#             if filename.endswith(".jpg") or filename.endswith('.png'):     
-#                 results = model(Image.open(os.path.join(image_folder, filename)))
-
-#                 for result in results:
-#                     boxes = result.boxes.cpu().numpy()
-#                     class_id = boxes.cls[0]
-
-#                     try:
-#                         float_id = float(class_id)
-#                         int_id = int(float_id)
-#                         class_ids.append(int_id)
-#                     except ValueError:
-#                         print("algo salió mal")
-
-#         filename = "emotion_results_{}.json".format(user_id)
-
-#         if os.path.exists(filename):
-#             with open(filename, mode='r') as f:
-#                 prev_data = json.load(f)
-
-#                 prev_data.extend(class_ids)
-
-#             with open(filename, mode='w') as actualized_file:
-#                 json.dump(prev_data, actualized_file)
-#         else:
-#             with open(filename, mode='w') as new_file:
-#                 json.dump(class_ids, new_file)
-
-#         return jsonify("el archivo JSON ha sido creado correctamente")
-
-#     except Exception as e:
-#         print(f"Error: {str(e)}")
-#         return jsonify({"error": "Hubo un problema durante el procesamiento de las imágenes."}), 500
-
-@router.route("/set_stadistics/<int:user_id>")
-def get_results(user_id):
-    filename= "emotion_results_{}.json".format(user_id)
-    
-    emotions_dict = {0: "angry", 1: "disgust", 2: "fear", 3: "happy", 4: "neutral", 5: "sad", 6: "surprice"}
     
     try:
-        with open(filename, mode='r') as file_read:
-            data=json.load(filename)
-            
-        counter = Counter(data)
-        results = []
-        
-        for emotion_id, count in counter.items():
-            emotion = emotions_dict[emotion_id]
-            average = count / len(data)
-            print(f'El promedio de {emotion} es: {average:.2f}')
-            results.append(f'El promedio de {emotion} es: {average:.2f}')
-            
-        
-        with open('resultados.txt', 'w') as data_to_save:
-            data_to_save.write('\n'.join(results))
-        
-        return jsonify("resultados txt ha sido creado correctamente")
+        #model = YOLO('./models/emilio_v1.pt')
+        image_folder = "images_/captured_frames/{}".format(user_id)
+
+        for filename in os.listdir(image_folder):
+            if filename.endswith(".jpg") or filename.endswith('.png'):     
+                """results = model(Image.open(os.path.join(image_folder, filename)))
+
+                for result in results:
+                    boxes = result.boxes.cpu().numpy()
+                    class_id = boxes.cls[0]
+
+                    try:
+                        float_id = float(class_id)
+                        int_id = int(float_id)
+                        class_ids.append(int_id)
+                    except ValueError:
+                        print("algo salió mal") """
+        class_ids = []
+        temp_list = [4.0, 2.0, 3.0, 3.0, 3.0, 4.0, 3.0, 2.0, 3.0]
+        # Agregamos la lista temp_list a class_ids
+        class_ids.extend(temp_list)
+
+        print("class_ids después de agregar temp_list:", class_ids)  # Mensaje de depuración
+
+        filename = "emotion_results_{}.json".format(user_id)
+        print("Nombre del archivo JSON:", filename)  # Mensaje de depuración
+
+        data_to_save = class_ids
+        if os.path.exists(filename):
+            with open(filename, mode='r') as f:
+                prev_data = json.load(f)
+                print("Datos previos cargados del archivo JSON:", prev_data)  # Mensaje de depuración
+            data_to_save = prev_data + class_ids
+
+        with open(filename, mode='w') as file:
+            json.dump(data_to_save, file)
+            print("Datos escritos en el archivo JSON")  # Mensaje de depuración
+
+        return jsonify("el archivo JSON ha sido creado o actualizado correctamente")
 
     except Exception as e:
         print(f"Error: {str(e)}")
         return jsonify({"error": "Hubo un problema durante el procesamiento de las imágenes."}), 500
-        
+@router.route("/set_statistics/<int:user_id>", methods=['GET'])
+def get_results(user_id):
+    filename = "emotion_results_{}.json".format(user_id)
+    print(f"Verificando el archivo JSON: {filename}")  # Depuración: Verificar la ruta del archivo
+
+    emotions_dict = {0: "angry", 1: "disgust", 2: "fear", 3: "happy", 4: "neutral", 5: "sad", 6: "surprise"}
+
+    try:
+        if not os.path.exists(filename):
+            print(f"Archivo no encontrado: {filename}")  # Depuración: Verificar si el archivo existe
+            return jsonify({"error": "Archivo JSON no encontrado"}), 404
+
+        with open(filename, mode='r') as file_read:
+            data = json.load(file_read)  
+            print(f"Datos leídos del archivo JSON: {data}")  # Depuración: Verificar datos leídos
+
+        counter = Counter(data)
+        results = []
+
+        for emotion_id, count in counter.items():
+            emotion = emotions_dict.get(int(emotion_id), "unknown")
+            average = count / len(data)
+            print(f'El promedio de {emotion} es: {average:.2f}')  # Depuración: Mostrar cálculos
+            results.append(f'El promedio de {emotion} es: {average:.2f}')
+
+        resultados_path = 'resultados.txt'
+        with open(resultados_path, 'w') as data_to_save:
+            data_to_save.write('\n'.join(results))
+            print(f"Resultados escritos en: {os.path.abspath(resultados_path)}")  # Depuración: Confirmar escritura
+
+        return jsonify("resultados.txt ha sido creado correctamente")
+
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        print(error_message)
+        return jsonify({"error": error_message}), 500
     
-@router.route("/send_mail/<int:user_id>/<int:videostream_id>", methods=['GET'])
-def send_email(user_id):
-    def get_email(user_id):
+@router.route("/send_mail/<int:video_id>", methods=['GET'])
+def send_email(video_id):
+    def get_email(video_id):
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute(f"SELECT email FROM user_basics WHERE id={user_id}")
+        cursor.execute(f"SELECT user_id FROM video WHERE id = {video_id}")
+        user_id = cursor.fetchone()
+        cursor.execute(f"SELECT email FROM user_basics WHERE id={user_id[0]}")
         email = cursor.fetchone()
         cursor.close()
         conn.close()
-        return email
+        return email[0] if email else None
+    email = get_email(video_id)
     
     gmail_user = 'hermestrimegisto4@gmail.com'  # Reemplaza esto por tu correo electrónico de Gmail
     gmail_password = 'cycm pdlp hkke ixlj'
@@ -177,7 +193,7 @@ def send_email(user_id):
 
         msg = MIMEMultipart()
         msg['From'] = gmail_user
-        msg['To'] = 'washingtonc0023@gmail.com'  # Reemplaza esto por el correo electrónico del destinatario
+        msg['To'] = email  # Reemplaza esto por el correo electrónico del destinatario
         msg['Subject'] = 'Resultados'
 
         # Adjuntar el archivo de texto al correo electrónico
@@ -189,7 +205,10 @@ def send_email(user_id):
         text = msg.as_string()
         server.sendmail(gmail_user, 'washingtonc0023@gmail.com', text)  # Reemplaza esto por el correo electrónico del destinatario
         server.close()
-        jsonify('correo enviado')
+        return jsonify({'message': 'correo enviado'})  # Asegúrate de devolver una respuesta aquí
     except Exception as e:
         logging.error(f"Error al enviar el correo electrónico: {e}")
-        return jsonify({"error": f"Algo salió mal: {str(e)}"}), 500
+        return jsonify({"error": f"Algo salió mal: {str(e)}"}), 500  # Devuelve una respuesta también en caso de error
+
+    # Es importante tener un retorno final en caso de que ninguna de las anteriores se ejecute
+    return jsonify({"error": "Error desconocido"}), 500
--- camera.js
+++ camera.js
@@ -1,12 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // Obtener el videoId del almacenamiento local
   const videoId = localStorage.getItem('videoId');
   if (videoId) {
     console.log('El videoId recuperado del almacenamiento local es:', videoId);
-    // Puedes hacer algo con el videoId aquí
   } else {
     console.log('No se encontró el videoId en el almacenamiento local.');
-    // Maneja la situación si no hay un videoId disponible
   }
 
   const videoElement = document.getElementById('videoElement');
@@ -15,18 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
   let videoStream;
   let captureInterval;
 
-  // Asegúrate de que cualquier función que necesite videoId se llame después de esta línea
-  // para asegurarte de que videoId está definido.
-
   startButton.addEventListener('click', () => {
     startCapturing();
-    // Si necesitas usar videoId en esta función, está disponible aquí.
-    
   });
 
   stopButton.addEventListener('click', () => {
     stopCapturing();
-    startProcess(videoId); // Asumiendo que deseas usar videoId aquí
+    startProcess(videoId);
   });
 
   async function startCapturing() {
@@ -42,7 +34,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const context = canvas.getContext('2d');
     context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
     const frameData = canvas.toDataURL('image/png');
-    // Enviar el frame capturado al proceso principal usando la API expuesta en preload.js
     window.api.saveFrame(frameData, videoId);
   }
 
@@ -53,68 +44,73 @@ window.addEventListener('DOMContentLoaded', () => {
     videoElement.srcObject = null;
   }
 
-});
- 
+  function startProcess(videoId) {
+    loadUserImage(videoId)
+      .then(() => processImageWithModel(videoId))
+      .then(() => setStatistics(videoId))
+      .then(() => sendEmail(videoId))
+      .catch(error => {
+        console.error('Error en el proceso:', error);
+      });
+  }
 
-      fetch("http://localhost:5000/load_img/${videoId}")
-      .then((response) => {
-          if (response.ok) {
-              return response.json();
-          }
+  function loadUserImage(videoId) {
+    return fetch(`http://localhost:5000/load_img/${videoId}`)
+      .then(response => response.ok ? response.json() : Promise.reject('Error al cargar la imagen'))
+      .then(data => {
+        console.log('Imagen cargada:', data);
+        return data;
       })
-        .then(data => {
-            console.log('Imagen cargada:', data);
-            return data;
-       })
-        .catch(error => {
-         console.error('Error al cargar la imagen:', error);
-       });
+      .catch(error => {
+        console.error('Error al cargar la imagen:', error);
+      });
+  }
 
-    function processImageWithModel(videoI) {
-      return fetch(`http:localhost:5000/load_model/${videoI}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Modelo procesado:', data);
-          return data;
-        })
-        .catch(error => {
-          console.error('Error al procesar la imagen con el modelo:', error);
-        });
-    }
+  function processImageWithModel(videoId) {
+    return fetch(`http://localhost:5000/load_model/${videoId}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Modelo procesado:', data);
+        return data;
+      })
+      .catch(error => {
+        console.error('Error al procesar la imagen con el modelo:', error);
+      });
+  }
 
-    function setStatistics(videoI) {
-      return fetch(`http:localhost:5000/set_stadistics/${videoI}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Estadísticas establecidas:', data);
-          return data;
-        })
-        .catch(error => {
-          console.error('Error al establecer estadísticas:', error);
-        });
-    }
+  function setStatistics(videoId) {
+    return fetch(`http://localhost:5000/set_statistics/${videoId}`)
+      .then(response => {
+        if (!response.ok) {
+            throw new Error('Respuesta del servidor no fue exitosa');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Estadísticas establecidas:', data);
+        return data;
+      })
+      .catch(error => {
+        console.error('Error al establecer estadísticas:', error.message);
+      });
+}
 
-    function sendEmail(videoI, videoStreamId) {
-      return fetch(`http:localhost:5000/send_mail/${videoI}/${videoStreamId}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Correo electrónico enviado:', data);
-          return data;
-        })
-        .catch(error => {
-          console.error('Error al enviar el correo electrónico:', error);
-        });
-    }
 
-    function startProcess(videoId) {
-      const videoStreamId = 1;  //Este valor también debe ser dinámico según necesites
-
-      loadUserImage(videoId)
-        .then(() => processImageWithModel(videoId))
-        .then(() => setStatistics(videoId))
-        .then(() => sendEmail(videoId, videoStreamId))
-        .catch(error => {
-          console.error('Error en el proceso:', error);
-        });
-    }
-
+function sendEmail(videoId) {
+  return fetch(`http://localhost:5000/send_mail/${videoId}`)
+    .then(response => {
+      if (!response.ok) {
+          // Lanza un error si la respuesta del servidor no es exitosa
+          throw new Error('Respuesta del servidor no fue exitosa');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Correo electrónico enviado:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error al enviar el correo electrónico:', error.message);
+    });
+  }
+});
